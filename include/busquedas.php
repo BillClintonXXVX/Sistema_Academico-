ab8b85423069e59e407a3d04f5c2d72bb2e5e65e
BillClintonXXVX <btaipecasapia@gmail.com>
2022-11-09 13:47:58 -0500

    Update busquedas.php
    hacer las consultas respectivas para cada pagina requerida

--- include/busquedas.php
+++ include/busquedas.php
@@ -41,5 +41,77 @@ function buscarDatosInst($conexion){
     return mysqli_query($conexion, $sql);
 }
 
+function mostrarPeriodoAcad($conexion){
+    $sql = "SELECT * FROM periodo_academico";
+    return mysqli_query($conexion, $sql);
+}
 
+function mostrarPresentePeriodoAcad($conexion){
+    $sql = "SELECT * FROM presente_periodo_acad";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarProgramaEstudios($conexion){
+    $sql = "SELECT * FROM programa_estudios";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarCalificacion($conexion){
+    $sql = "SELECT * FROM calificaciones";
+    return mysqli_query($conexion, $sql);
+}
+function mostrarCargo($conexion){
+    $sql = "SELECT * FROM cargo";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarCondicion($conexion){
+    $sql = "SELECT * FROM condicion";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarGenero($conexion){
+    $sql = "SELECT * FROM genero";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarDocente($conexion){
+    $sql = "SELECT * FROM docente";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarMatricula($conexion){
+    $sql = "SELECT * FROM matricula";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarSemestre($conexion){
+    $sql = "SELECT * FROM semestre";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarUnidDidacticas($conexion){
+    $sql = "SELECT * FROM unidad_didactica";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarUserDocente($conexion){
+    $sql = "SELECT * FROM usuarios_docentes";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarUserEstudiante($conexion){
+    $sql = "SELECT * FROM usuarios_estudiante";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarProgrUnidDidac($conexion){
+    $sql = "SELECT * FROM programacion_unidad_didactica";
+    return mysqli_query($conexion, $sql);
+}
+
+function mostrarModulosFormativos($conexion){
+    $sql = "SELECT * FROM modulo_profesional";
+    return mysqli_query($conexion, $sql);
+}
 ?>
