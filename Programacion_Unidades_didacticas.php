8a827936c8e8be78121c348938fd60d150c2e065
BillClintonXXVX <btaipecasapia@gmail.com>
2022-11-09 13:44:09 -0500

    Update Programacion_Unidades_didacticas.php
    cargar programacion unidades didacticas

--- Programacion_Unidades_didacticas.php
+++ Programacion_Unidades_didacticas.php
@@ -41,7 +41,7 @@ include "include/verificar_sesion.php";
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Relacion de Estudiantes</h2>
+                    <h2>Programacion De Unidades Didacticas</h2>
                     <ul class="nav navbar-right">
                       <li>
                         <a href="estudiante.php" class="btn btn-success">Agregar Nuevo</a>
@@ -55,29 +55,31 @@ include "include/verificar_sesion.php";
                       <thead>
                         <tr>
                           <th>Id</th>
-                          <th>DNI</th>
-                          <th>APELLIDOS Y NOMBRES</th>
-                          <th>DIRECCION</th>
-                          <th>CARRERA</th>
-                          <th>SEMESTRE</th>
+                          <th>Id Unidad Didactica</th>
+                          <th>Id Docente</th>
+                          <th>Id Periodo Academico</th>
+                          <th>Id Programa Estudio</th>
+                          <th>Id Semestre</th>
+                          <th>Cantidad Calificacion</th>
                           <th>Acciones</th>
                         </tr>
                       </thead>
                       <tbody>
                         <?php 
-                        $b_estudiantes = buscarEstudiantes($conexion);
-                        while ($res_b_estudiantes = mysqli_fetch_array($b_estudiantes)) {
+                        $b_progr_unid_didac = mostrarProgrUnidDidac($conexion);
+                        while ($res_b_progr_unid_didac = mysqli_fetch_array($b_progr_unid_didac)) {
                         ?>
                         <tr>
-                          <td><?php echo $res_b_estudiantes['id']; ?></td>
-                          <td><?php echo $res_b_estudiantes['dni']; ?></td>
-                          <td><?php echo $res_b_estudiantes['apellidos_nombres'];  ?></td>
-                          <td><?php echo $res_b_estudiantes['direccion']; ?></td>
-                          <td><?php echo $res_b_estudiantes['id_programa_estudios']; ?></td>
-                          <td><?php echo $res_b_estudiantes['id_semestre']; ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['id']; ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['id_unidad_didactica']; ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['id_docente'];  ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['id_periodo_acad']; ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['id_programa_estudio']; ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['id_semestre']; ?></td>
+                          <td><?php echo $res_b_progr_unid_didac['cant_calificacion']; ?></td>
                           <td>
-                            <a href="editar_estudiante.php?id=<?php echo $res_b_estudiantes['id']; ?>" class="btn btn-primary">Editar</a>
-                            <a href="operaciones/eliminar_estudiante.php?id=<?php echo $res_b_estudiantes['id']; ?>" class="btn btn-danger">Eliminar</a>
+                            <a href="editar_estudiante.php?id=<?php echo $res_b_progr_unid_didac['id']; ?>" class="btn btn-primary">Editar</a>
+                            <a href="operaciones/eliminar_estudiante.php?id=<?php echo $res_b_progr_unid_didac['id']; ?>" class="btn btn-danger">Eliminar</a>
                           </td>
                         </tr>
                         <?php
