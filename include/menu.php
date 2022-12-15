ebb4f9b3e7ad867e2dc7fc289c1da73bb92886ce
BillClintonXXVX <btaipecasapia@gmail.com>
2022-12-15 10:56:51 -0500

    actualización

--- include/menu.php
+++ include/menu.php
@@ -12,7 +12,7 @@
             <!-- menu profile quick info -->
             <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="img/logo iestp.png" alt="..." class="img-circle profile_img">
+                <img src="img/user.png" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
                 <span>Bienvenido,</span>
