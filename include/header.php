ede924186ead966a5fb2d0eb25f233324c81df56
BillClintonXXVX <btaipecasapia@gmail.com>
2022-12-15 13:41:09 -0500

    actualización

--- include/header.php
+++ include/header.php
@@ -11,7 +11,7 @@ include "verificar_sesion.php";
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Encabezado! | </title>
+    <title>Gentelella Alela! | </title>
     <link rel="shortcut icon" href="img/logo iestp.png">
     <!-- Bootstrap -->
     <link href="Gentella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
